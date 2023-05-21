12d0a92d50c17ca2ef1208d05aa9ee7852b826cd
Sushil456 <amisushil469@gmail.com>
2023-05-21 20:48:33 +0530

    React AI project example

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom"
+import {logo} from './assets'
+import { CreatePosts, Home } from "./pages"
+export const App =()=>{
+  return(
+    // implement Browser Router 
+   <BrowserRouter>
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+        {/* Creating two link for routing from home and Creat post */}
+        <Link to="/">
+          <img src ={logo} alt ="logo"
+          className="w-28 object-contain "/>
+        </Link>
+
+        <Link to="/create-post"
+        className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">
+          Create
+        </Link>
+      </header>
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-posts" element={<CreatePosts />} />
+        </Routes>
+      </main>
+   </BrowserRouter>
+  )
+}
+
+
+// main url = "https://youtu.be/EyIvuigqDoA"
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import { Home } from "./Home";
+import { CreatePosts } from "./CreatePosts";
+
+// exporting them into one object 
+export{
+    Home, CreatePosts
+}
